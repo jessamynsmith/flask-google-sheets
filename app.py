6e7cbc561ed5a78190efce5480184cb72396dad2
TRezendes <78620355+TRezendes@users.noreply.github.com>
2022-12-27 02:57:19 +0000

    App.py now uses config.json instead of env variabl

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-import os
 from flask import Flask, render_template
 import googleapiclient.discovery
 from google.oauth2 import service_account
+import json
 
 
+
+def get_credentials():
+    
+    '''
+    app.config['SECRET_KEY'] = config.get('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = config.get('DEV_DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config.get('SQLALCHEMY_TRACK_MODIFICATIONS')  
+    '''
+    scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+    GOOGLE_PRIVATE_KEY = config.get('GOOGLE_PRIVATE_KEY')
+    
+    account_info = {
+      "private_key": GOOGLE_PRIVATE_KEY,
+      "client_email": config.get('GOOGLE_CLIENT_EMAIL'),
+      "token_uri": "https://accounts.google.com/o/oauth2/token",
+    }
+    
+    credentials = service_account.Credentials.from_service_account_info(account_info, scopes=scopes)
+    return credentials
+
+'''
 def get_credentials():
     scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
     GOOGLE_PRIVATE_KEY = os.environ["GOOGLE_PRIVATE_KEY"]
@@ -18,7 +39,7 @@ def get_credentials():
 
     credentials = service_account.Credentials.from_service_account_info(account_info, scopes=scopes)
     return credentials
-
+'''
 
 def get_service(service_name='sheets', api_version='v4'):
     credentials = get_credentials()
@@ -28,12 +49,16 @@ def get_service(service_name='sheets', api_version='v4'):
 
 app = Flask(__name__)
 
+# Load configuration from JSON file
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
 
 @app.route('/', methods=['GET'])
 def homepage():
     service = get_service()
-    spreadsheet_id = os.environ["GOOGLE_SPREADSHEET_ID"]
-    range_name = os.environ["GOOGLE_CELL_RANGE"]
+    spreadsheet_id = config.get('GOOGLE_SPREADSHEET_ID')
+    range_name = config.get('GOOGLE_CELL_RANGE')
 
     result = service.spreadsheets().values().get(
         spreadsheetId=spreadsheet_id, range=range_name).execute()
